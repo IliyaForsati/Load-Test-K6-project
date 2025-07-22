8e579894b56c0e2a067a3658e493f05483f5c242
Iliya Forsati <iliyaforsati2005@gmail.com>
2025-07-22 16:07:11 +0330

    handle exeption in main func

--- loadtest.js
+++ loadtest.js
@@ -2,7 +2,7 @@ import * as APIs from './APISenders/APIs.js';
 import { randomInt } from './util.js';
 import { getCurrentUser, logger } from './main_methods.js';
 
-export const url = "http://rentx.npgnasr.com";
+export const url = "http://192.168.1.54:8051";
 
 // set up
 export const options = {
@@ -13,15 +13,20 @@ export const options = {
 
 // main func
 export default function () {
-  const getAllRes = APIs.Order.getAllSummary_post_Req(url);
+  try {
+    const getAllRes = APIs.Order.getAllSummary_post_Req(url);
 
-  const orderNoList = JSON.parse(getAllRes.body).model.gridData.data.map(row => row[0]);
+    const orderNoList = JSON.parse(getAllRes.body).model.gridData.data.map(row => row[0]);
 
-  const randomRecNo = randomInt(0, orderNoList.length);
+    const randomRecNo = randomInt(0, orderNoList.length);
 
-  const getRecNumRes = APIs.Order.getRecNo_get_req(url, randomRecNo);
+    const getRecNumRes = APIs.Order.getRecNo_get_req(url, randomRecNo);
 
-  const _POSTRes = APIs.OrderRevision._post_req(url, randomRecNo);
-  
+    const _POSTRes = APIs.OrderRevision._post_req(url, randomRecNo);
+  }
+  catch (err) {
+    console.log(err);
+  }
+
   logger();
 }
