2f1472d31c7b3cec5e724d861ed44d3718bcd1c0
Iliya Forsati <iliyaforsati2005@gmail.com>
2025-07-22 17:36:47 +0330

    debug

--- APISenders/OrderRevisionAPIs.js
+++ APISenders/OrderRevisionAPIs.js
@@ -3,18 +3,18 @@ import * as JSONFiles from '../resquests/index.js'
 
 // POST: api/OrderRevision
 export function _post_req(url, recNo) {
-const variables = {
-    base_url: url,
-    token: funcs.getToken(),
-    recNo: recNo,
-};
+    const variables = {
+        base_url: url,
+        token: funcs.getToken(),
+        recNo: recNo,
+    };
 
-const body = JSON.stringify({
-    orderNo: recNo,
-    saveRevision: true,
-});
+    const body = JSON.stringify({
+        orderNo: recNo,
+        saveRevision: true,
+    });
 
-const res = funcs.sendRequest(JSONFiles._post, variables, body);
+    const res = funcs.sendRequest(JSONFiles._post, variables, body);
 
-return res;
+    return res;
 }
